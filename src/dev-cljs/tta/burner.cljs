32b928d24c10e48aed059f1f821f9a72369c534b
Kavita Vaish <kava@topsoe.com>
2018-04-06 11:09:31 +0530

    Burner table component tf

--- src/dev-cljs/tta/burner.cljs
+++ src/dev-cljs/tta/burner.cljs
@@ -5,7 +5,8 @@
             [stylefy.core :as stylefy :refer [use-style use-sub-style]]
             [ht.style :refer [color color-hex color-rgba]]
             [tta.app.icon :as ic]
-            [tta.app.comp :as app-comp]))
+            [tta.app.comp :as app-comp]
+            [tta.app.scroll :as scroll]))
 
 (defonce my-state (r/atom {}))
 
@@ -82,29 +83,40 @@ inset -3px -3px 10px rgba(0,0,0,0.3)"}
        [sf-burner-popup {:state state}]])))
 
 (def tf-burner-style
-  (let [circle {:width "24px", :height "24px"
-                :position "absolute"
-                :top "12px", :left "12px"
+  (let [circle {:width "32px", :height "32px"
+                :display "inline-block"
                 :border-radius "50%"
-                :background (color-hex :white)
-                :box-shadow "inset -6px -6px 10px rgba(0,0,0,0.3)"}
+                :font-size "10px"
+                :line-height "32px"
+                :text-align "center"
+                :vertical-align "top"
+                :margin-top "8px"
+                :color (color-hex :white)}
         palette-unit {}]
-    {:height "48px", :width "96px"
-     :display "inline-block"
-     :background "#eee"
-     :position "relative"
+    {:height   "48px", :width "120px"
      ::stylefy/sub-styles
-     {:circle (assoc circle
-                :top "12px", :left "12px"
-                :box-shadow "-3px -3px 10px 3px rgba(0,0,0,0.3)")
-      :palette {:width "48px"}
-      :palette-unit {:display "inline-block" :width "24px" :height "50px"}
-      :temp-scale {:vertical-align "top" :margin-left "5px" :font-size "12px"}}}))
+               {:circle       circle
+                :palette      {:width "48px"}
+                :palette-unit {:display "inline-block"
+                               :width "24px", :height "50px"}
+                :temp-scale   {:vertical-align "top"
+                               :margin-left "5px", :font-size "12px"}}}))
+(def opening-color-map
+  {15 (color-hex :red)
+   30 (color-hex :orange)
+   45 (color-hex :green)
+   60 (color-hex :teal)
+   75 (color-hex :indigo)
+   90 (color-hex :royal-blue)})
 
 (defn opening->color [opening]
   "r 255 => 0, g 0 => 255 => 0, b 0 => 255 "
   (if (and opening (not= opening ""))
-    (let [distribution [15 30 45 60 75 90]]
+    (some (fn [i]
+            (if (<= opening i)
+              (get opening-color-map i)))
+          (keys opening-color-map))
+    #_(let [distribution [15 30 45 60 75 90]]
       (some (fn [i]
               (if (<= opening i)
                 (let [r (js/Math.floor (- 255 (* i 2.84)))
@@ -114,41 +126,72 @@ inset -3px -3px 10px rgba(0,0,0,0.3)"}
                       b (js/Math.floor (* 2.84 i))]
                   (str "rgb(" r "," g "," b ")"))
                 nil)) distribution))
-    "rgb(255,255,255)"))
+    (color-hex :slate-grey)))
 
 (defn color-palette []
   (let [style tf-burner-style
         distribution [90 75 60 45 30 15]]
     [:div (stylefy/use-sub-style style :palette)
-     (map (fn [i]
-            [:div {:style {:height "50px"}}
-             [:div (merge (stylefy/use-sub-style style :palette-unit)
-                          {:style {:background (opening->color i)}})]
-             [:span (stylefy/use-sub-style style :temp-scale) (str i "°")]])
-          distribution)
+     (doall
+       (map (fn [i] ^{:key i}
+       [:div {:style {:height "50px"}}
+        [:div (merge (stylefy/use-sub-style style :palette-unit)
+                     {:style {:background (opening->color i)}})]
+        [:span (stylefy/use-sub-style style :temp-scale) (str i "°")]])
+            distribution))
      [:span (merge (stylefy/use-sub-style style :temp-scale)
                    {:style {:float "right"
                             :padding-right "6px"
-                            }}) "0°"]]))
+                            :margin-top "-7px"}}) "0°"]]))
 
-(defn tf-burner [{:keys [value on-change] :as props}]
+(defn tf-burner [{:keys [value-fn label-fn on-change row col] :as props}]
   (let [state (r/atom props)
-        style tf-burner-style]
-    (fn [{:keys [value]}]
-      [:div (stylefy/use-style style)
-       [:div (merge (stylefy/use-sub-style style :circle)
-                    {:style {:background (opening->color (:value @state))}})]
-       [:span {:style {:margin-left "35px"}}
-        [app-comp/text-input
-         {:width     40
-          :value     (:value @state)
-          :on-change (fn [val]
-                       (if (<= 0 val 90)
-                         (swap! state assoc :value val)))}]]])))
+        style tf-burner-style
+        on-change #(on-change row col %)]
+    (fn [{:keys [value-fn label-fn]}]
+      (let [value (value-fn row col)
+            label (label-fn row col)]
+        [:div (stylefy/use-style style)
+         [:div (merge (stylefy/use-sub-style style :circle)
+                      {:style {:background (opening->color value)}})
+          label]
+         [app-comp/text-input
+          {:width     48
+           :value     value
+           :on-change on-change}]]))))
+
+(defn burner-table-tf
+  [width height row-count col-count value-fn label-fn on-change ]
+  [:div {:style {:height 300 :padding "10px"}}
+   [:div {:style {:font-weight "600"}} "Burner Rows"] [:br]
+   [:div {:style {:width "80%" :float "left"}}
+    [scroll/table-grid
+     {:height              height :width width
+      :row-header-width    0 :col-header-height 30
+      :row-count           row-count :col-count col-count
+      :row-height          48 :col-width 120
+      :table-count         [1 1]
+      :padding             [3 3 15 15]
+      :row-header-renderer (fn [row [t-row t-col]]
+                             nil)
+      :col-header-renderer (fn [col [t-row t-col]]
+                             [:div {:style {:text-align "center"}}
+                              (inc col)])
+      :cell-renderer       (fn [row col [t-row t-col]]
+                             [tf-burner {:value-fn  value-fn
+                                         :label-fn  label-fn
+                                         :on-change on-change}])}]]
+   [:div {:style {:width "15%" :float "left" :margin-left "20px"}}
+    [color-palette]]])
+(defn burner-tf-comp []
+  [burner-table-tf
+   500 300 10 7
+   #(js/console.log %1 %2)
+   #(js/console.log %1 %2)
+   #(js/console.log %1 %2 %3) 400])
 
 (defn burner []
-  [:div {:style {:height 300
-                 :padding "50px"}}
+  #_[:div {:style {:height 300 :padding "50px"}}
    [color-palette]
    [:div
     {:style {:float "left"}}
@@ -160,4 +203,5 @@ inset -3px -3px 10px rgba(0,0,0,0.3)"}
                 :dual-nozzle? true}] [:br]
 
     [tf-burner {:value     (get-in @my-state [:tf :burner])
-                :on-change #(swap! my-state assoc-in [:tf :burner] %)}]]])
+                :on-change #(swap! my-state assoc-in [:tf :burner] %)}]]]
+  [burner-tf-comp])
