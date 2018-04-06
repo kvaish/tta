9d7b54b8ae72db16a8acd6b2d7ff71f9dd4a4fd5
Kavita Vaish <kava@topsoe.com>
2018-04-06 15:12:23 +0530

    burner table component sf

--- src/dev-cljs/tta/burner.cljs
+++ src/dev-cljs/tta/burner.cljs
@@ -10,78 +10,6 @@
 
 (defonce my-state (r/atom {}))
 
-;; 48x48
-(def sf-burner-style
-  (let [col-on (color-hex :green)
-        col-off (color-hex :red)
-        col-off-aux (color-hex :orange)
-        col-off-fix (color-hex :brown)
-        circle {:width "24px", :height "24px"
-                :position "absolute"
-                :top "12px", :left "12px"
-                :border-radius "50%"
-                :background (color-hex :white)
-                :box-shadow "inset -6px -6px 10px rgba(0,0,0,0.3)"}]
-    {:height "48px", :width "48px"
-     :display "inline-block"
-     :background "#eee"
-     :position "relative"
-     ::stylefy/sub-styles
-     {:on (assoc circle :background col-on)
-      :off (assoc circle :background col-off)
-      :off-aux (assoc circle :background col-off-aux)
-      :off-fix (assoc circle :background col-off-fix)
-      :popup {:position "absolute"
-              :height "96px", :width "96px"
-              :top "-24px", :left "-24px"
-              :z-index "99999"
-              :border-radius "50%"
-              :background (color-rgba :white 0 0.5)
-              :box-shadow "0 0 10px 3px rgba(0,0,0,0.3),
-inset -3px -3px 10px rgba(0,0,0,0.3)"}
-      :circle (assoc circle
-                     :top "36px", :left "36px"
-                     :box-shadow "-3px -3px 10px 3px rgba(0,0,0,0.3)")}}))
-
-(defn sf-burner-popup [{:keys [state]}]
-  (let [style sf-burner-style
-        {:keys [value on-change hover? dual-nozzle?]} @state]
-    (if hover?
-      (into [:div (stylefy/use-sub-style style :popup)
-             [:div (stylefy/use-sub-style style :circle)]
-             [:div (update (stylefy/use-sub-style style (keyword (or value "off")))
-                           :style assoc
-                           :top "6px", :left "36px")]]
-            (->> (if dual-nozzle?
-                   [[:on "42px" "6px"]
-                    [:off "42px" "66px"]
-                    [:off-fix "64px" "22px"]
-                    [:off-aux "64px" "50px"]]
-                   [[:on "48px" "8px"]
-                    [:off "48px" "64px"]
-                    [:off-fix "66px" "36px"]])
-                 (map (fn [[k t l]]
-                        [:div (-> (stylefy/use-sub-style style k)
-                                  (update :style assoc :top t, :left l
-                                          :cursor "pointer")
-                                  (assoc :on-click #(let [value (name k)]
-                                                      (swap! state assoc :value value)
-                                                      (on-change value))))])))))))
-
-;; 48x48
-(defn sf-burner [{:keys [value on-change] :as props}]
-  (let [style sf-burner-style
-        state (r/atom props)
-        mouse-enter #(swap! state assoc :hover? true)
-        mouse-leave #(swap! state assoc :hover? false)]
-    (fn [{:keys [value]}]
-      (swap! state assoc :value value)
-      [:div (merge (stylefy/use-style style)
-                   {:on-mouse-enter mouse-enter
-                    :on-mouse-leave mouse-leave})
-       [:div (stylefy/use-sub-style style (keyword (or value "off")))]
-       [sf-burner-popup {:state state}]])))
-
 (def tf-burner-style
   (let [circle {:width "32px", :height "32px"
                 :display "inline-block"
@@ -160,6 +88,76 @@ inset -3px -3px 10px rgba(0,0,0,0.3)"}
            :value     value
            :on-change on-change}]]))))
 
+(def sf-burner-style
+  (let [col-on (color-hex :green)
+        col-off (color-hex :red)
+        col-off-aux (color-hex :orange)
+        col-off-fix (color-hex :brown)
+        circle {:width "24px", :height "24px"
+                :position "absolute"
+                :top "24px", :left "24px"
+                :border-radius "50%"
+                :background (color-hex :white)
+                :box-shadow "inset -6px -6px 10px rgba(0,0,0,0.3)"}]
+    {:height "48px", :width "48px"
+     :display "inline-block"
+     :position "relative"
+     ::stylefy/sub-styles
+     {:on (assoc circle :background col-on)
+      :off (assoc circle :background col-off)
+      :off-aux (assoc circle :background col-off-aux)
+      :off-fix (assoc circle :background col-off-fix)
+      :popup {:position "relative"
+              :height "96px", :width "96px"
+              :top "-12px", :left "-12px"
+              :z-index "99999"
+              :border-radius "50%"
+              :background (color-rgba :white 0 0.5)
+              :box-shadow "0 0 10px 3px rgba(0,0,0,0.3),
+inset -3px -3px 10px rgba(0,0,0,0.3)"}
+      :circle (assoc circle
+                :top "36px", :left "36px"
+                :box-shadow "-3px -3px 10px 3px rgba(0,0,0,0.3)")}}))
+
+(defn sf-burner-popup [{:keys [state]}]
+  (let [style sf-burner-style
+        {:keys [value on-change hover? dual-nozzle?]} @state]
+    (if hover?
+      (into [:div (stylefy/use-sub-style style :popup)
+             [:div (stylefy/use-sub-style style :circle)]
+             [:div (update (stylefy/use-sub-style style (keyword (or value "off")))
+                           :style assoc
+                           :top "6px", :left "36px")]]
+            (->> (if dual-nozzle?
+                   [[:on "42px" "6px"]
+                    [:off "42px" "66px"]
+                    [:off-fix "64px" "22px"]
+                    [:off-aux "64px" "50px"]]
+                   [[:on "48px" "8px"]
+                    [:off "48px" "64px"]
+                    [:off-fix "66px" "36px"]])
+                 (map (fn [[k t l]]
+                        [:div (-> (stylefy/use-sub-style style k)
+                                  (update :style assoc :top t, :left l
+                                          :cursor "pointer")
+                                  (assoc :on-click #(let [value (name k)]
+                                                      (swap! state assoc :value value)
+                                                      (on-change value))))])))))))
+
+;; 48x48
+(defn sf-burner [{:keys [value on-change] :as props}]
+  (let [style sf-burner-style
+        state (r/atom props)
+        mouse-enter #(swap! state assoc :hover? true)
+        mouse-leave #(swap! state assoc :hover? false)]
+    (fn [{:keys [value]}]
+      (swap! state assoc :value value)
+      [:div (merge (stylefy/use-style style)
+                   {:on-mouse-enter mouse-enter
+                    :on-mouse-leave mouse-leave})
+       [:div (stylefy/use-sub-style style (keyword (or value "off")))]
+       [sf-burner-popup {:state state}]])))
+
 (defn burner-table-tf
   [width height row-count col-count value-fn label-fn on-change ]
   [:div {:style {:height 300 :padding "10px"}}
@@ -183,12 +181,47 @@ inset -3px -3px 10px rgba(0,0,0,0.3)"}
                                          :on-change on-change}])}]]
    [:div {:style {:width "15%" :float "left" :margin-left "20px"}}
     [color-palette]]])
+
 (defn burner-tf-comp []
   [burner-table-tf
    500 300 10 7
    #(js/console.log %1 %2)
    #(js/console.log %1 %2)
    #(js/console.log %1 %2 %3) 400])
+
+(defn burner-table-sf
+  [width height row-count col-count value-fn on-change ]
+  [:div {:style {:height 300 :padding "10px"}}
+   [scroll/table-grid
+    {:height              height :width width
+     :row-header-width    64 :col-header-height 30
+     :row-count           row-count :col-count col-count
+     :row-height          64 :col-width 64
+     :labels              ["Row" "Burner"]
+     :gutter              [20 20]
+     :table-count         [1 2]
+     :padding             [3 3 15 15]
+     :row-header-renderer (fn [row [t-row t-col]]
+                            [:div {:style {:text-align "center"
+                                           :line-height "64px"
+                                           :border-right "1px solid"
+                                           :height "inherit"}}
+                             (inc row)])
+     :col-header-renderer (fn [col [t-row t-col]]
+                            [:div {:style {:text-align "center"
+                                           :border-bottom "1px solid"
+                                           :height "inherit"}}
+                             (inc col)])
+     :cell-renderer       (fn [row col [t-row t-col]]
+                             [sf-burner {:value-fn     value-fn
+                                         :on-change    on-change
+                                         :dual-nozzle? true}])}]])
+
+(defn burner-sf-comp []
+  [burner-table-sf
+   700 300 10 15
+   #(js/console.log %1 %2 %3)
+   #(js/console.log %1 %2 %3 %4) 400])
 
 (defn burner []
   #_[:div {:style {:height 300 :padding "50px"}}
@@ -204,4 +237,6 @@ inset -3px -3px 10px rgba(0,0,0,0.3)"}
 
     [tf-burner {:value     (get-in @my-state [:tf :burner])
                 :on-change #(swap! my-state assoc-in [:tf :burner] %)}]]]
-  [burner-tf-comp])
+  #_[burner-tf-comp]
+
+  [burner-sf-comp])
